1bbe0ada5f3f346799082d1b13ed01e33b80c907
Drozd Denis <pavelzuravkov@MacBook-Pro.Dlink>
2024-03-16 10:01:38 +0300

    Add new working api

--- Weather/Sources/Screens/Weather/WeatherInteractor.swift
+++ Weather/Sources/Screens/Weather/WeatherInteractor.swift
@@ -36,7 +36,7 @@ final class WeatherInteractor: WeatherInteractorProtocol {
     }
 
     func fetchData(parameter: String, completion: @escaping (Result<Weather, NetworkError>) -> Void) {
-        guard let url = URL(string: "https://api.weatherapi.com/v1/forecast.json?key=7891af3d376b447a895105303242702&q=\(parameter)&days=7&aqi=no&alerts=no") else { return }
+        guard let url = URL(string: "https://api.weatherapi.com/v1/forecast.json?key=2b2a831937964f21bf464404241603&q=\(parameter)&days=7&aqi=no&alerts=no") else { return }
         
         let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
             if error != nil {
@@ -70,7 +70,7 @@ final class WeatherInteractor: WeatherInteractorProtocol {
     }
     
     func searchCity(with: String) {
-        guard let url = URL(string: "https://api.weatherapi.com/v1/search.json?key=7891af3d376b447a895105303242702&q=\(with)") else { return }
+        guard let url = URL(string: "https://api.weatherapi.com/v1/search.json?key=2b2a831937964f21bf464404241603&q=\(with)") else { return }
         
         let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
             if error != nil {
